c46298266c89ffb3a10d3cb489e6bf28de0e21cf
Abhay Pandey <pandeyabhay055@gmail.com>
2026-03-11 18:19:33 +0530

    all setup configured

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+
+
+
+(async function (localFilePath) {
+
+// Configuration
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+
+
+    // Upload an image
+    const uploadResult = await cloudinary.uploader
+        .upload(
+            localFilePath, {
+            public_id: 'shoes',
+        }
+        )
+        .catch((error) => {
+            console.log(error);
+        });
+
+    console.log(uploadResult);
+
+})
+
+
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
